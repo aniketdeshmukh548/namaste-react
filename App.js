d0b9a1350e080dac8a9eff18a534dc10de519fa8
aniketdeshmukh548 <aniketdeshmukh548@gmail.com>
2023-12-28 19:17:54 +0530

    Episode 03-Laying the foundation

--- App.js
+++ App.js
@@ -1,27 +1,45 @@
-/* <div id='parent'>
-    <div id='child'>
-        <h1>I am H1 Tag</h1>
-        <h2>I am H2 Tag</h2>
-    </div>
-    <div id='child2'>
-        <h1>I am H1 Tag</h1>
-        <h2>I am H2 Tag</h2>
-    </div>
-</div> */
-
-//----------------------create above structure using react
 import React from "react";
-import ReactDOM  from "react-dom/client";
-//The below code is the core react code but html is easy so we use JSX instead of core react
-const heading=React.createElement(('div'),{id:'parent'},
-    [(React.createElement(('div'),{id:'child'},(
-        [React.createElement(('h1'),{},('I am H1 Tag!!')),React.createElement(('h2'),{},('I am H2 Tag!!'))]
-    ))),
-    (React.createElement(('div'),{id:'child2'},(
-        [React.createElement(('h1'),{},('I am H1 Tag!!')),React.createElement(('h2'),{},('I am H2 Tag!!'))]
-    )))]
+import ReactDOM from 'react-dom/client';
+
+//React Element
+// const heading= (
+//     <h1>Namaste react from react element</h1>
+// );
+
+//react composition or Component composition- here we combine 2 or more react components inside one another.
+
+// const Title=()=>(
+//     <h1>This is Namaste Title for Component composition!!</h1>
+// )
+
+// const HeadingComp=()=>(
+//     <div id="container">
+//         <Title />
+//         <h3 className="heading">Namaste React from heading component w/o return keyword!!</h3>
+//     </div>
+// )
+
+//normal variable injected into react element and it is injected in  react component.
+const elem=1000; //noram varaible
+
+const title=(        //react elemnet
+    <h1>This is Namaste Title for Component composition!!
+      {elem } 
+    </h1> //noraml element injected inside react element
 )
 
-//const heading=React.createElement(('h1'),{id:"heading"},('Hello World From React'));
-const root=ReactDOM.createRoot(document.getElementById('root'))
-root.render(heading)
+const HeadingComp=()=>(
+    <div id="container">
+        {title}
+        <h3 className="heading">Namaste React from heading component w/o return keyword!!</h3>
+    </div>  //react elemet injected inside react component
+)
+//React Functional component
+// const HeadingComp=()=>{
+//     return (
+//         <h1 id="heading">Namaste from react component with return keyword!!</h1>
+//     )
+// }
+const root=ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<HeadingComp />)
